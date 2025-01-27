2561535f325c113dca390d81505532a20deebf3d
Keshav Sharma <kassassin99@gmail.com>
2025-01-27 10:16:05 +0530

    onFocus Event:

--- keshav-test/src/App.js
+++ keshav-test/src/App.js
@@ -2,28 +2,16 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState("");
+  const handleFocus = () => {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    alert("Input Focused!");
 
-    alert(value);
-
-    setValue(""); // Clear the input field by setting the state to an empty string
   };
+
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+       <input type="text" onFocus={handleFocus} placeholder="Focus on me" />
     </>
   );
 }
