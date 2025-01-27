33ff92585e5d3d7733c63da66835e201587e55f1
Keshav Sharma <kassassin99@gmail.com>
2025-01-27 10:15:23 +0530

    form submit and clear form event

--- keshav-test/src/App.js
+++ keshav-test/src/App.js
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    alert("Form Submitted!");
+    alert(value);
+
+    setValue(""); // Clear the input field by setting the state to an empty string
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter text" />
+        <input
+          type="text"
+          placeholder="Enter text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
 
         <button type="submit">Submit</button>
       </form>
