7b08e84c316bbce36628d4ea9b91b4ae3e33fe43
Keshav Sharma <kassassin99@gmail.com>
2025-01-27 10:11:00 +0530

    onKeyDown Event

--- keshav-test/src/App.js
+++ keshav-test/src/App.js
@@ -1,39 +1,17 @@
-import React, { useState } from 'react';
 
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("Mouse Over Me");
+  const handleKeyDown = () => {
 
-  const handleMouseOver = () => {
-    setText("Thank You");
+    alert("Key Pressed!");
+
   };
 
-  const handleMouseOut = () => {
-    setText("Mouse Over Me");
-  };
 
   return (
     <>
-      <div
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        style={{
-          backgroundColor: "#D94A38",
-
-          width: "120px",
-
-          height: "20px",
-
-          padding: "40px",
-
-          textAlign: "center",
-
-          cursor: "pointer",
-        }}
-      >
-        {text}
-      </div>
+     <input onKeyDown={handleKeyDown} placeholder="Press any key" />
     </>
   );
 }
