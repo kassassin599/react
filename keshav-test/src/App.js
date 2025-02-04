c1389362b2aae6b125f52cd78ae6315280d70f9b
Keshav Sharma <kassassin99@gmail.com>
2025-02-04 09:45:39 +0530

    smart books react app

--- keshav-test/src/App.js
+++ keshav-test/src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>Our employees</h1>
-      <Counter from={0} to={500} duration={3000} />
+      <Counter from={100} to={500} duration={3000} />
       <h1>Our projects</h1>
-      <Counter from={0} to={1200} duration={3000} />
+      <Counter from={0} to={1200} duration={10000} />
     </div>
   );
 }
